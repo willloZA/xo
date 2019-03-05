9ac4b9099ab90b6c97636275b19e6183bee8375d
Lloyd Williams <lloydnexus5x@gmail.com>
2019-03-05 22:10:45 +0200

    implemented room functionality

--- app/noughts-crosses.controller.js
+++ app/noughts-crosses.controller.js
@@ -1,19 +1,55 @@
 function noughtsAndCrossesController ($rootScope, socket) {
   let ctrl = this;
   //binary record of players moves
-  ctrl.playerBoard = 0;
-  ctrl.mySymbol = ['X','0'];
-  ctrl.multiplayer = true;
-  ctrl.myTurn = true;
-  ctrl.rcvdMove = false;
-  ctrl.remainingBoard = [0,1,2,3,4,5,6,7,8]
+  function reset() {
+    ctrl.mySymbol       = ['X','0'];
+    ctrl.remainingBoard = [0,1,2,3,4,5,6,7,8]
+    ctrl.playerBoard    = 0;
+    ctrl.joinRoomNum    = null;
+    ctrl.createRoomNum  = null;
+    ctrl.roomNum        = null;
+    ctrl.multiplayer    = false;
+    ctrl.myTurn         = true;
+    ctrl.rcvdMove       = false;
+  }
 
   ctrl.$onInit = () => {
     socket.on('connect', function () {
       console.log('connected!');
     });
 
+    reset();
+
   }
+
+  socket.on('joined-room', function (d) {
+    console.log(d);
+    ctrl.roomNum      = d.room;
+    ctrl.mySymbol     = d.symbol;
+    ctrl.multiplayer  = true;
+    // X first turn 0 second turn
+    if (d.symbol[0] === '0') ctrl.myTurn = false
+  });
+
+  socket.on('alert', function (d) {
+    // alert or modal alert message on opponent disconnect
+    console.log(d);
+  });
+
+  socket.on('opponent-disconnected', function (d) {
+    // alert or modal alert message on opponent disconnect
+    console.log(d)
+    socket.emit('leave-room');
+    reset();
+  });
+
+  ctrl.createRoom = () => {
+    socket.emit('create-room');
+  };
+
+  ctrl.joinRoom = () => {
+    socket.emit('join-room', parseInt(ctrl.joinRoomNum));
+  };
 
   ctrl.mpMoveEmit = (id) => {
     console.log('mp : ' + id);
--- server/socket/socket.controller.js
+++ server/socket/socket.controller.js
@@ -6,8 +6,11 @@
 
 module.exports = (_io, db) => {
 
-  const io      = _io,
-        redisDB = db;
+  const io          = _io,
+        {promisify} = require('util'),
+        redisDB     = db,
+        getAsync    = promisify(redisDB.get).bind(redisDB),
+        setAsync    = promisify(redisDB.set).bind(redisDB);
   
   const winCombination = [
     [1,2,3], [4,5,6], [7,8,9],
@@ -30,24 +33,109 @@ module.exports = (_io, db) => {
 
   let resetGames = () => {
     redisDB.set('totalPlayers',0);
-    redisDB.set('totalRooms',1);
+    redisDB.set('totalRooms',0);
     redisDB.set('allRooms', JSON.stringify({
-      emptyRooms: [1],
+      emptyRooms: [],
       fullRooms : []
     }));
   }
 
-  let allRooms      = null,
-      totalPlayers  = null;
-
   resetGames();
 
-  let room = 'testRoom';
+  /* Figure out if values external from connection are necessary
+  let totalRooms    = null,
+      availRooms    = null,
+      totalPlayers  = null; */
+  let assignedRooms = {};
+
 
   io.on('connection', (socket) => {
     console.log(socket.id + ' connected!');
-    
+    redisDB.incr('totalPlayers');
+
+
+
+    socket.on('create-room', () => {
+      // install util.promisify-all and apply to redis client
+      Promise.all(['totalRooms','allRooms'].map((key) => getAsync(key)))
+        .then((arr) => {
+          let totalRooms = parseInt(arr[0]),
+              rooms      = JSON.parse(arr[1]);
+          if (totalRooms <= 100) {
+            let id = 1;
+            while (id <= 100) {
+              let existsEmpty = rooms.emptyRooms.includes(id);
+              let existsFull  = rooms.fullRooms.includes(id);
+              if (!existsEmpty && !existsFull) {
+                break
+              }
+              id++;
+            }
+            totalRooms++
+            rooms.emptyRooms.push(id);
+            redisDB.incr('totalRooms');
+            redisDB.set('allRooms', JSON.stringify(rooms));
+            console.log(socket.id+ ' joining room-'+id+' as X');
+            socket.join('room-'+id);
+            socket.emit('joined-room',{
+              room: id,
+              symbol: ['X','0']
+            });
+            assignedRooms[socket.id] = id;
+          } else {
+            socket.emit('alert',{ alert: 'The maximum number of existing rooms has been reached'});
+          }
+        });
+    });
+
+    socket.on('join-room', (id) => {
+      getAsync('allRooms')
+        .then((str)=> {
+          let rooms = JSON.parse(str);
+          if (rooms.fullRooms.includes(id)) {
+            socket.emit('alert',{ alert: 'The room specified is already full'});
+          } else if (rooms.emptyRooms.includes(id)) {
+            rooms.emptyRooms.splice(rooms.emptyRooms.indexOf(id), 1);
+            rooms.fullRooms.push(id);
+            redisDB.set('allRooms', JSON.stringify(rooms));
+            console.log(socket.id+ ' joining room-'+id+' as 0');
+            socket.join('room-'+id);
+            socket.emit('joined-room',{
+              room: id,
+              symbol: ['0','X']
+            });
+            assignedRooms[socket.id] = id;
+          } else {
+
+            socket.emit('alert',{ alert: `The room specified doesn't exist`});
+          }
+        });
+    });
+
+    socket.on('leave-room', () => {
+      let roomId = 'room-'+assignedRooms[socket.id];
+      let id     = assignedRooms[socket.id];
+      delete assignedRooms[socket.id];
+      socket.leave(roomId);
+      getAsync('allRooms')
+        .then((str) => {
+          let rooms = JSON.parse(str);
+          rooms.fullRooms.splice(rooms.fullRooms.indexOf(id), 1);
+          redisDB.set('allRooms', JSON.stringify(rooms));
+        });
+    });
+
+    socket.on('disconnect', () => {
+      console.log(socket.id + ' disconnected!');
+      if (assignedRooms[socket.id]) {
+        let roomId = 'room-'+assignedRooms[socket.id];
+        delete assignedRooms[socket.id];
+        io.to(roomId).emit('opponent-disconnected',{ alert: `Your opponent has disconnected`});
+      }
+      redisDB.decr('totalPlayers');
+    });
   });
+
 
   return io;
 
